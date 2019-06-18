1f5dcd200eafbd96b6a1e3fce71704cd52b40606
Xiaoming Sun <rsun2014@gmail.com>
2019-06-18 13:10:09 -0700

    Inital maven project add git ignore

---
+++ src/main/java/pers/xiaoming/scala/App.java
@@ -0,0 +1,13 @@
+package pers.xiaoming.scala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

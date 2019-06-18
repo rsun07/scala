8ae505145e65200723871a22e57aea8a4c19db96
Xiaoming Sun <rsun2014@gmail.com>
2019-06-18 13:48:40 -0700

    Basic while loop

---
+++ src/main/java/pers/xiaoming/scala/basic_learning/condition_and_loop/BasicWhileLoop.scala
@@ -0,0 +1,11 @@
+package pers.xiaoming.scala.basic_learning.condition_and_loop
+
+object BasicWhileLoop {
+  def main(args: Array[String]): Unit = {
+    var n = 10
+    while(n > 0) {
+      println(n)
+      n -= 1
+    }
+  }
+}

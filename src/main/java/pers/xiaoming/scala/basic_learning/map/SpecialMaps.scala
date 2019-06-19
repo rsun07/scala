7f30ae5d49e0bee80736318cb91ced04d2d157ce
Xiaoming Sun <rsun2014@gmail.com>
2019-06-18 21:21:37 -0700

    special maps

---
+++ src/main/java/pers/xiaoming/scala/basic_learning/map/SpecialMaps.scala
@@ -0,0 +1,12 @@
+package pers.xiaoming.scala.basic_learning.map
+
+object SpecialMaps {
+  def main(args: Array[String]): Unit = {
+    val sortedMap = scala.collection.immutable.SortedMap("Mike" -> 1, "Jack" -> 2, "Jane" -> 3)
+    for((key, value) <- sortedMap) println(key + ":" + value)
+
+    println
+    val linkedHashMap = scala.collection.mutable.LinkedHashMap("Mike" -> 1, "Jack" -> 2, "Jane" -> 3)
+    for((key, value) <- linkedHashMap) println(key + ":" + value)
+  }
+}

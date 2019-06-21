17e019be7fa474f7dc1e5d92e7217c0c2294fd35
Xiaoming Sun <rsun2014@gmail.com>
2019-06-20 19:38:06 -0700

    Currying func

---
+++ src/test/java/pers/xiaoming/scala/basic_learning/functional_programming/basic/CurryingDemo.scala
@@ -0,0 +1,22 @@
+package pers.xiaoming.scala.basic_learning.functional_programming.basic
+
+import org.junit.Test
+
+class CurryingDemo {
+  @Test
+  def demo: Unit = {
+    log("INFO", "Hello")
+
+    log2("INFO2")("Hello world")
+
+    log2("INFO3")("Hello world !")
+  }
+
+  def log(level:String, msg:String): Unit = {
+    println(level + " : " + msg)
+  }
+
+  def log2(level:String) = (msg:String) => println(level + " : " + msg)
+
+  def log3(level:String)(msg:String) = println(level + " : " + msg)
+}

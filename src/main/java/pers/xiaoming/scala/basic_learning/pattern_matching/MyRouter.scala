1fbecd7843af5e4221145ce73b88efbb794ca73a
Xiaoming Sun <rsun2014@gmail.com>
2019-06-21 16:07:59 -0700

    List pattern matching

--- src/main/java/pers/xiaoming/scala/basic_learning/pattern_matching/MyRouter.scala
+++ src/main/java/pers/xiaoming/scala/basic_learning/pattern_matching/MyRouter.scala
@@ -6,7 +6,19 @@ object MyRouter {
       case Array("health") => "/my/health"
       case Array("name") => "/my/name/{name}"
       case Array("name", "class") => "/my/class/{class}/name/{name}"
-      case Array("class", _*) => "/my/class/{class}"
+      case Array("class", _subpath) => "/my/class/{class}/" + _subpath
+      case Array("class", _*) => "/my/class/{class}/help"
+      case _ => "/404"
+    }
+  }
+
+  def route(urls:List[String]): String = {
+    urls match {
+      case "health"::Nil => "/my/health"
+      case "name"::Nil => "/my/name/{name}"
+      case "name"::"class"::Nil => "/my/class/{class}/name/{name}"
+      case "class"::_subpath::Nil => "/my/class/{class}/" + _subpath
+      case "class"::tail => "/my/class/{class}" + tail.toString()
       case _ => "/404"
     }
   }
